340d3cccb728dc5a4241a3709a34b03571cd180c
Davi Zanlorensi <davi.s.z@outlook.com.br>
2026-09-25 20:48:50 -0300

    env

--- config/config.cjs
+++ config/config.cjs
@@ -1,10 +1,9 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_KEY,
-    "host": process.env.DB_SERVER,
-    "port": process.env.DB_PORT,
+    "username": "root",
+    "password": "hope2023",
+    "database": "hope",
+    "host": "localhost",
     "dialect": "mysql"
   },
   "test": {
@@ -16,11 +15,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "avnadmin",
-    "password": "AVNS_H0CvJHTC7qukbOZkBIO",
-    "database": "defaultdb",
-    "host": "hope-prod-hope.b.aivencloud.com",
-    "port": "17589",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_KEY,
+    "host": process.env.DB_SERVER,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   }
 }
